b042e7b356a6586b9ad0bd57002017c710fcae6b
Datteln <85793841+Datteln@users.noreply.github.com>
2021-06-16 21:14:18 +0800

    Update z.java

--- z.java
+++ z.java
@@ -9,5 +9,8 @@ public class z {
         System.out.println("hello,git,master test commit");
         System.out.println("hello,git,ideal test commit");
         System.out.println("hello,git,ideal test commit");
+        System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
     }
 }
